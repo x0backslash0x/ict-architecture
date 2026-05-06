fead17b4c0ad0cf7b8b5608914aca34b85271b38
Patrick Lanove <patrick.lanove@student.ap.be>
2026-05-06 10:52:52 +0200

    labo210 auth aanvraag doen

--- labo210/authenticator/server.js
+++ labo210/authenticator/server.js
@@ -6,8 +6,13 @@ const home = `http://${hostname}:${port}`
 
 app.get('/', (req, res) => {
   const authURL = "https://github.com/login/oauth/authorize";
-  res.redirect(authURL);
+  const client_id = "Ov23liJaiLGqKAM0XaTC"
+  const redirect_url = `${authURL}?client_id=${client_id}&redirect_url=${home}`
+  console.log("redirecting to " + redirect_url)
+  res.redirect(redirect_url); // BUG: moet een andere route zijn, zit nu in een loop
 });
+
+// in een andere route (callback) de code uit het response object tonen op de pagina
 
 app.listen(port, hostname, () => {
   console.log(`server listening on ${home}`);
