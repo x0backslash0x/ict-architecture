90d3a7d53917cf242528b6d0faedebfe83a51423
x0backslash0x <patrick.lanove@student.ap.be>
2026-05-26 17:06:56 +0200

    labo208 api calls toevoegen aan gateway

--- labo208/gateway/gateway.py
+++ labo208/gateway/gateway.py
@@ -1,19 +1,26 @@
 #gateway.py
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
 
 @app.get("/customers")
 def get_customers():
+    response = httpx.get("http://customers:3000/get_customer_data_by_email", params={'email': 'rsands0@dell.com'})
+    return response.json()
     customer_data = "customer data"
     return customer_data
 
 @app.get("/orders")
 def get_orders():
+    response = httpx.get("http://orders:3000/get_order_data")
+    return response.json()
     order_data = "order data"
     return order_data
 
 @app.get("/products")
 def get_products():
+    response = httpx.get("http://products:3000/get_product_data")
+    return response.json()
     product_data = "product data"
     return product_data
--- readme.md
+++ readme.md
@@ -1,1 +1,25 @@
 Repository voor labo oefeningen bij OLOD ICT Architecture
+
+# Labo 8 (bis jaar)
+## Gebruik
+| Service | endpoint |
+| ------- | -------- |
+| customers | localhost:3000/get_customer_data_by_email?email=\<email\> |
+| orders | localhost:3001/get_order_data |
+| products | localhost:3002/get_product/data |
+| gateway/customers | localhost:3003/customers |
+| gateway/orders | localhost:3003/orders |
+| gateway/products | localhost:3003/products |
+
+**omgeving klaar zetten**
+```bash
+docker compose build
+docker compose up
+```
+
+**services aanroepen**
+```bash
+curl localhost:3003/customers
+curl localhost:3003/orders
+curl localhost:3003/products
+```
