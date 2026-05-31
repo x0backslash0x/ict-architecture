896e817d83463f3674d981847320504e3676d01f
= <=>
2026-05-31 14:57:30 +0200

    labo208-3 product service uitbreiden met delete route

--- labo208/products/index.js
+++ labo208/products/index.js
@@ -51,6 +51,31 @@ app.post("/add_product_data", async (req, res) => {
     }
 });
 
+// product data verwijderen (op basis van id)
+app.delete("/remove_product_data_by_id", async (req, res) => {
+    // %20 vertalen in witruimte voor MySQL compatibiliteit
+    const product_id = decodeURI(req.query.product_id);
+
+    if (!product_id) {
+        return res.status(400).json({ error: "id is vereist" });
+    }
+
+    try {
+        console.log("Deleting product with id " + product_id)
+        const query = `DELETE FROM Products WHERE id = "${product_id}"`
+        const [result] = await db.query(query);
+
+        if (result.affectedRows == 0) {
+            return res.status(400).json({ error: "query failed"})
+        }
+
+        res.status(200).json(result);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "database error"});
+    }
+});
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
