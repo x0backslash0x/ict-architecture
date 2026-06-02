35adf367f7f36bf640b2b1127d7d2f44ea68a596
x0backslash0x <patrick.lanove@student.ap.be>
2026-06-02 16:21:16 +0200

    labo205 aparte service maken voor receiver

--- labo205/helloworld/receive.py
+++ labo205/helloworld/receive.py
@@ -4,7 +4,7 @@
 import pika, sys, os
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
     channel = connection.channel()
 
     channel.queue_declare(queue='hello', durable=True, arguments={'x-queue-type': 'quorum'})
